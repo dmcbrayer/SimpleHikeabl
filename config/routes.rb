0588cc8d3f8f5b82e820ece47d834b265032b0b2
Daniel McBrayer <dmcbrayer@gmail.com>
2014-06-28 11:02:26 -0400

    Added users controller with index and show actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
 
-  devise_for :users
-  root 'pages#home'
 
+  devise_for :users
+  resources :users, only: [:index, :show]
+  root 'pages#home'
   get 'about' => 'pages#about'
 
   # The priority is based upon order of creation: first created -> highest priority.
