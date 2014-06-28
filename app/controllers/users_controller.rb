0d4e26ce85559447e78c241009ee26dddba782a1
Daniel McBrayer <dmcbrayer@gmail.com>
2014-06-28 11:06:50 -0400

    added before_action authentication to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-	before_action :set_user, only: [:show]
+	before_action :authenticate_user!
+  before_action :set_user, only: [:show]
 
   def index
   	@users = User.all
