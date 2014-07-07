15709959d0d358e01d8e8e05dad7f7ef2977ad3c
Daniel McBrayer <dmcbrayer@gmail.com>
2014-07-07 15:59:44 -0400

    Fixed up the forms and got rid of simple_form code.  Also added default image for users.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,14 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_attached_file :avatar, :styles => { :header => "24x24#", :small => "52x52#", :medium => "300x300#", :thumb => "100x100#" }
+  has_attached_file :avatar, 
+  	:styles => { 
+  		:header => "24x24#", 
+  		:small => "52x52#", 
+  		:medium => "300x300#", 
+  		:thumb => "100x100#" }, 
+  		:default_url => "/images/default_user_:style.jpg"
+  
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
 
